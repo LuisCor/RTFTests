f9eb3419ff7644804c0adfd8d3e82a38c670e199
Luis Correia <lcorreia@imaginarycloud.com>
2022-03-10 13:08:20 +0000

    Adding lights

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Canvas } from "react-three-fiber";
 import "./styles.css";
+import * as THREE from "three";
 
 
 export default function App() {
@@ -8,6 +9,22 @@ export default function App() {
     <>
       <div className="App">
         <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
+          <ambientLight intensity={0.1} />
+          <spotLight
+            intensity={1.3}
+            angle={0.3}
+            penumbra={1}
+            position={[5, 25, 20]}
+            shadow-bias={-0.0001}
+          />
+          <spotLight
+            intensity={1.3}
+            angle={1}
+            penumbra={1}
+            position={[20, 10, 0]}
+          >
+            <primitive object={new THREE.AxesHelper(10)} />
+          </spotLight>
           <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -2, 0]}>
             <planeBufferGeometry attach="geometry" args={[25, 15]} />
             <meshPhongMaterial attach="material" color="gray" />
